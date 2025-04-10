7b31b3f89250025ce8727d3527fd16cd9bbc48cd
sleepyturtlee <127636103+sleepyturtlee@users.noreply.github.com>
2025-04-09 18:15:53 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -11,17 +11,16 @@ public class Digits
 		if(num == 0) {
 			digitList.add(0);
 		}
-		int n = num;
-		while(n > 0) {
-			digitList.add(0, n%10);
-			n = n/10;
+		while(num > 0) {
+			digitList.add(0, num%10);
+			num = num/10;
 		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		for(int i = 0; i < digitList.size()-1; i++) {
-			if(digitList.get(i+1) < digitList.get(i)) {
+			if(digitList.get(i+1) <= digitList.get(i)) {
 				return false;
 			}
 		}
